022ab59cfa47b2a9f4c7a122862a49e86c10fab3
Ariel Szabó <arielszabo@outlook.com>
2021-03-03 14:41:31 +0100

    Version5

--- home/basic4.js
+++ home/basic4.js
@@ -2,3 +2,4 @@ let text = 'The most valuable player of the match is ';
 let players = ['Martin', 'Thomas', 'Peter', 'Mathias', 'Niki']
 let textPeter = text + players[2] + '.';
 console.log(textPeter);
+
