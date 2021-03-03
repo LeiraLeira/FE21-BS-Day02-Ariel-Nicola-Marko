97e19d76d1f1604b792b153dab80665309493a9c
Nikola Dimic <nikoladimic666@gmail.com>
2021-03-03 14:24:49 +0100

    advanced

---
+++ Advanced/advanced1.js
@@ -0,0 +1,37 @@
+var a = 7;
+
+var b = 14;
+
+var c = "21";
+
+var d = "36.66";
+
+var e = 42;
+
+var numbers = a + b + Number(c) + parseInt(d) + e ;
+
+
+console.log(numbers);
+
+
+//multiply the numbers
+
+var f = '1';
+
+var g = 15;
+
+var h = 8;
+
+var i = "1";
+
+ var multyple = Number(f) * g * h * Number(i);
+
+ console.log(multyple);
+
+
+
+
+ //Divide the two results that you got from the arithmetic operations and print the result in the web browser.
+
+ var result = numbers / multyple;
+document.write(result);
